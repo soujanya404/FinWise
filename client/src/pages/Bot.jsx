9881e8ddc8f66e8051f4dc61f3b4d459ae88418c
RaghavendraKushtagi <2022cs_raghavendrakushtagi_c@nie.ac.in>
2025-03-06 19:03:58 +0530

    Commit 2

--- client/src/pages/Bot.jsx
+++ client/src/pages/Bot.jsx
@@ -27,7 +27,7 @@ function Bot() {
     setLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:5000/', {
+      const response = await axios.post('https://finwise-advisor.onrender.com/', {
         message: input,
         level: expertiseLevel
       });
@@ -52,7 +52,7 @@ function Bot() {
 
   const clearChat = async () => {
     try {
-      await axios.post('http://localhost:5000/clear-history');
+      await axios.post('https://finwise-advisor.onrender.com/clear-history');
       setMessages([]);
     } catch (error) {
       console.error('Error clearing chat history:', error);
